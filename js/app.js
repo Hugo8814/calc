5ae8989bb76d0e8bbdcc9d2e243f4a8335dc6a80
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-02-09 23:53:31 +0000

    damm

--- js/app.js
+++ js/app.js
@@ -1,25 +1,51 @@
 const display = document.getElementById("display");
 const num = document.querySelectorAll(".num");
 const ops = document.querySelectorAll(".ops");
+const equals = document.querySelector(".calc__numPad__btn-equals");
+const resets = document.querySelector(".calc__numPad__btn-resets");
+const del = document.querySelector(".calc__numPad__btn-del");
 
-const displayNum = function (number) {
-  display.value += number;
+function calc() {
+  try {
+    const result = eval(currentInput);
+    display.value = result;
+    currentInput = result.toString();
+  } catch (error) {
+    display.value = "Error";
+    console.error(error);
+  }
+}
+
+ops.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonText = button.textContent.trim();
+    let operator;
+    if (buttonText === "x") {
+      // Use '*' internally for calculation
+      operator = "*";
+    } else if (/[+\-*/%.]/.test(buttonText)) {
+      // If it's any other operator, assign the button's text directly
+      operator = " " + buttonText + " ";
+    } else {
+      // If it's a number or any other character, parse it as a float
+      operator = parseFloat(buttonText);
+    }
+
+    displayNum(buttonText);
+    console.log(buttonText); // Display the button's text ('x' will be displayed)
+    currentInput += operator; // Use the corresponding operator for calculation
+  });
+});
+let currentInput = "";
+const displayNum = function (value) {
+  currentInput += value;
+  display.value += value;
 };
 
 const clear = function () {
+  currentInput = "";
   display.value = "";
 };
-
-const calc = function () {
-  try {
-    const result = eval(display.value);
-    display.value = result;
-  } catch (err) {
-    console.log(err);
-    display.value = "Error";
-  }
-};
-
 display.addEventListener("input", function () {
   validateInput(this);
 });
@@ -30,24 +56,16 @@ function validateInput(input) {
 
 num.forEach((button) => {
   button.addEventListener("click", () => {
-    const number = parseFloat(button.textContent.trim());
-
-    displayNum(number);
+    displayNum(button.textContent.trim());
   });
 });
 
-ops.forEach((button) => {
-  button.addEventListener("click", () => {
-    const buttonText = button.textContent.trim();
-    let number;
-    if (/[+\-*/%]/.test(buttonText)) {
-      // If it's an operator, assign the button's text directly
-      number = buttonText;
-    } else {
-      // If it's a number, parse it as a float
-      number = parseFloat(buttonText);
-    }
-
-    displayNum(number);
-  });
+resets.addEventListener("click", function () {
+  clear();
 });
+del.addEventListener("click", function () {
+  currentInput = currentInput.slice(0, -1);
+  display.value = display.value.slice(0, -1);
+});
+
+equals.addEventListener("click", calc);
